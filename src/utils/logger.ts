afae43476772e339461f45872ba5a9d0fc70f495
Victor Essien <viktoressien1@gmail.com>
2026-03-17 13:11:20 +0100

    feat: Implement the knexfile.ts for knexjs configuration. Created the tables

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -8,7 +8,7 @@ const logger = winston.createLogger({
     winston.format.splat(),
     winston.format.json()
   ),
-  defaultMeta: { service: 'artist-platform-api' },
+  defaultMeta: { service: 'Demo_credit wallet-service' },
   transports: [
     new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
     new winston.transports.File({ filename: 'logs/combined.log' }),
